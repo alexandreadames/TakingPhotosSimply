faa39d50d1d712707daaee15c6772525be3e07a1
alexandreadames <alexandre.adames@gmail.com>
2015-07-09 18:27:52 -0300

    Tirando fotos em uma aplicação android de forma simples
    Tirando fotos em uma aplicação android de forma simples

---
+++ TakingaPhotoSimply/app/src/main/java/com/androiddeveloperbrasil/takingaphotosimply/MainActivity.java
@@ -0,0 +1,74 @@
+package com.androiddeveloperbrasil.takingaphotosimply;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private ImageView ivPhoto;
+    private Button btTakeaaPhoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ivPhoto = (ImageView) findViewById(R.id.ivPhoto);
+        btTakeaaPhoto = (Button) findViewById(R.id.btTakeaPhoto);
+
+        btTakeaaPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ivPhoto.setImageBitmap(imageBitmap);
+        }
+    }
+}
